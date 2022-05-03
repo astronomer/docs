e0a83d569b17785416894a68d0c6ba45304b0d5d
Paola Peraza Calderon <paola@astronomer.io>
2022-05-02 23:08:41 -0400

    fix versions file

--- src/versions.js
+++ src/versions.js
@@ -1,11 +1,10 @@
 export const siteVariables = {
-  cli- 1.5.0
-cliVersion: '1.5.0',
+  cliVersion: '1.5.0',
   runtimeVersion: '5.0.0',
-    // Hacky variable so that we can use env var formatting in CI/CD templates
-    deploymentid: '${ASTRONOMER_DEPLOYMENT_ID}',
-      keyid: '${ASTRONOMER_KEY_ID}',
-        keysecret: '${ASTRONOMER_KEY_SECRET}',
-          // Hacky variable for droneci
-          deploymentiddrone: '$ASTRONOMER_DEPLOYMENT_ID',
+  // Hacky variable so that we can use env var fromatting in CI/CD templates
+  deploymentid: '${ASTRONOMER_DEPLOYMENT_ID}',
+  keyid: '${ASTRONOMER_KEY_ID}',
+  keysecret: '${ASTRONOMER_KEY_SECRET}',
+  // Hacky variable for droneci
+  deploymentiddrone: '$ASTRONOMER_DEPLOYMENT_ID',
 };
