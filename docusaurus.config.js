445dc6112af6d4c317bf8ef30befc729684a84e5
jwitz <jwitz@astronomer.io>
2022-06-16 09:56:01 -0600

    Please work

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -247,8 +247,8 @@ module.exports = {
       {
        id: 'software',
        changefreq: 'weekly',
-       priority: 0.3,
-       ignorePatterns: ['/software/0.28/**'],
+       priority: 0.5,
+       ignorePatterns: ['/software/0.28/**','/software/0.27/**','/software/0.26/**','/software/0.25/**','/software/0.23/**','/software/0.16/**'],
        filename: 'sitemap.xml',
       },
     ]
