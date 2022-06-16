35575b3a037a09dda138c69a5f85798753c26658
jwitz <jwitz@astronomer.io>
2022-06-16 10:23:50 -0600

    Limit number of patterns

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -240,7 +240,7 @@ module.exports = {
       {
        id: 'software',
        changefreq: 'daily',
-       ignorePatterns: ['/software/0.28/**','/software/0.27/**','/software/0.26/**','/software/0.25/**','/software/0.23/**','/software/0.16/**'],
+       ignorePatterns: ['/software/0.28/**'],
       },
     ]
   ],
