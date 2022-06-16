53de30d27daaff354482a7f8e38ef50b9532970c
jwitz <jwitz@astronomer.io>
2022-06-16 09:14:12 -0600

    Changefreq

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -244,7 +244,7 @@ module.exports = {
       '@docusaurus/plugin-sitemap',
       {
        id: 'software',
-       changefreq: 'weekly',
+       changefreq: 'daily',
        ignorePatterns: ['/software/0.28/**','/software/0.27/**','/software/0.26/**','/software/0.25/**','/software/0.23/**','/software/0.16/**'],
        filename: 'sitemap.xml',
       },
