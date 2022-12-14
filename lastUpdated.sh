f0bf2f090fecf9b984c7c2c32c2d96971c41538f
jwitz <jwitz@astronomer.io>
2022-12-14 17:51:48 +0000

    Load metrics

--- lastUpdated.sh
+++ lastUpdated.sh
@@ -26,7 +26,7 @@ function main {
         FILTER='tee'
     fi
 
-    ack_file_info $format | clean_input | sort_cleaned_input $reverse | clean_output | sleep | commit
+    ack_file_info $format | clean_input | sort_cleaned_input $reverse | clean_output | commit
 }
 
 # Use ack's -f flag to just list files. We could use pretty much anything here,
@@ -61,11 +61,8 @@ function clean_output {
     cut -f 2- >> .github/metrics/log.csv
 }
 
-function sleep {
-    sleep 2m
-}
-
 function commit {
+    wait
     git add .
     git config --global user.name "jwitz"
     git config --global user.email "jwitz@astronomer.io"
