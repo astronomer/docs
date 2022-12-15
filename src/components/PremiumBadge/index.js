ebb06883a37bab53411764b5905ce9bd2fdb1ab1
jwitz <jwitz@astronomer.io>
2022-12-15 17:19:35 +0000

    Update file path

--- src/components/PremiumBadge/index.js
+++ src/components/PremiumBadge/index.js
@@ -6,7 +6,7 @@ export default function LinkCard() {
   return (
     <Link className={styles.premiumBadge} id="premiumBadge">
         <div className={styles.premiumBadge}>
-          <img src="/img/PremiumLarge.svg" href="https://www.astronomer.io/pricing/" alt='This feature is an Astro Premium-tier service.' />
+          <img src="/img/PremiumLarge.png" href="https://www.astronomer.io/pricing/" alt='This feature is an Astro Premium-tier service.' />
         </div>
     </Link>
   )
