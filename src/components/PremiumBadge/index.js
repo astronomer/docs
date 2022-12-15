8ec9f65a27f3c955570a7fb5958441eee7642cf3
Katie Brady <96535736+katiebrady-astro@users.noreply.github.com>
2022-12-15 13:08:27 -0500

    Add title attr for hover text

--- src/components/PremiumBadge/index.js
+++ src/components/PremiumBadge/index.js
@@ -7,7 +7,7 @@ export default function LinkCard() {
     <Link className={styles.premiumBadge} id="premiumBadge">
         <div className={styles.premiumBadge}>
         <a target="_blank" href="https://www.astronomer.io/get-started/"> 
-            <img src="/img/PremiumLarge.png" alt='This feature is an Astro Premium-tier service.' /> 
+            <img src="/img/PremiumLarge.png" alt="This feature is an Astro Premium-tier service." title="This feature is an Astro Premium-tier service." /> 
         </a>
         </div>
     </Link>
