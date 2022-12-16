f779780c34b9e137ced1328fffa77d059eb169c5
Jake Witz <74574233+jwitz@users.noreply.github.com>
2022-12-16 03:57:30 -0700

    Update src/components/PremiumBadge/index.js

--- src/components/PremiumBadge/index.js
+++ src/components/PremiumBadge/index.js
@@ -7,7 +7,7 @@ export default function LinkCard() {
     <Link className={styles.premiumBadge} id="premiumBadge">
         <div className={styles.premiumBadge}>
         <a target="_blank" href="https://www.astronomer.io/get-started/"> 
-            <img src="/img/PremiumLarge.png" alt="This feature is available only with the Astro Premium plan." title="This feature is available only with the Astro Premium plan." /> 
+            <img src="/img/PremiumLarge.png" alt="This feature is available only with an Astro Premium plan." title="This feature is available only with an Astro Premium plan." /> 
         </a>
         </div>
     </Link>
