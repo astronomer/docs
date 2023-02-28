a18d57490f24b3b13843204ee844470f7de69c9b
TJaniF <tamara.fingerlin@astronomer.io>
2023-03-01 00:46:44 +0100

    fix mysterious sudden lint failure

--- code-samples/dags/dag-best-practices/bad_practices_dag_1.py
+++ code-samples/dags/dag-best-practices/bad_practices_dag_1.py
@@ -13,7 +13,6 @@ results = hook.get_records("SELECT * FROM grocery_list;")
 sql_queries = []
 
 for result in results:
-
     grocery = result[0]
     amount = result[1]
     sql_query = f"INSERT INTO purchase_order VALUES ('{grocery}', {amount});"
@@ -25,7 +24,6 @@ for result in results:
     start_date=datetime(2023, 1, 1), max_active_runs=3, schedule="@daily", catchup=False
 )
 def bad_practices_dag_1():
-
     insert_into_purchase_order_postgres = PostgresOperator.partial(
         task_id="insert_into_purchase_order_postgres",
         postgres_conn_id="postgres_default",
