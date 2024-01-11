91ae8d03d31f7027a728f5195a3a98a285788420
jwitz <jwitz@astronomer.io>
2024-01-11 17:20:07 +0000

    Upgrade Docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,6 +9,7 @@ module.exports = {
   noIndex: false,
   onBrokenLinks: 'throw', // 'warn' for drafts, 'throw' for prod
   onBrokenMarkdownLinks: 'throw',
+  onBrokenAnchors: 'warn',
   markdown: {
     mermaid: true,
     preprocessor: ({ filePath, fileContent }) => {
