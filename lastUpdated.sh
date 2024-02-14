89c30d62966b058b0f12c603149f045eac22acbd
jwitz <jwitz@astronomer.io>
2024-02-14 12:18:29 +0000

    Load metrics

--- lastUpdated.sh
+++ lastUpdated.sh
@@ -66,10 +66,10 @@ function clean_output {
 function commit {
     wait
     git add .
+    git checkout usage-action
     git config --global user.name "jwitz"
     git config --global user.email "jwitz@astronomer.io"
     git commit -a -m "Load metrics"
-    git push
 }
 
 main "$@"
