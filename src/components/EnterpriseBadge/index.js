9fb98996a61cdc02bd39e2f75dbfd87e66e45b21
jwitz <jwitz@astronomer.io>
2024-03-21 11:43:10 +0000

    Create pricing tier badges

---
+++ src/components/EnterpriseBadge/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import styles from './styles.module.css';
+
+export default function LinkCard() {
+  return (
+    <Link className={styles.enterpriseBadge} id="enterpriseBadge">
+        <div className={styles.enterpriseBadge}>
+        <a target="_blank" href="https://docs.astronomer.io/astro/astro-architecture">
+            <img src="/img/Badge_Enterprise.png" alt="This feature is available only if you are on the Enterprise tier or above." title="This feature is available only if you are on the Enterprise tier or above." />
+        </a>
+        </div>
+    </Link>
+  )
+}
