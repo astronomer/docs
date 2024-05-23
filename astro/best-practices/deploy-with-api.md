36f4688c55b141eb41cf6c867a31729d64486677
L. Zdanski <laura.zdanski@astronomer.io>
2024-05-23 10:05:33 -0400

    create files

---
+++ astro/best-practices/deploy-with-api.md
@@ -0,0 +1,6 @@
+---
+title: 'Deploy to Astro with the API'
+sidebar_label: 'Deploy with API'
+id: deploy-with-api
+---
+
--- sidebarsAstro.js
+++ sidebarsAstro.js
@@ -330,6 +330,7 @@ module.exports = {
         'best-practices/airflow-vs-astro-alerts',
         'best-practices/connections-branch-deploys',
         'best-practices/cross-deployment-dependencies',
+        'best-practices/deploy-with-api',
       ],
     },
     {
